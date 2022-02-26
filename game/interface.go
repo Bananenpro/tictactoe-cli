56f6abdad26d8fd9010e243a7d322a76d789397b
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-02-26 10:09:47 +0100

    Improve field input

--- game/interface.go
+++ game/interface.go
@@ -14,7 +14,7 @@ func ClearTerminal() {
 }
 
 func PrintBoard(board board) {
-	fmt.Printf("\n %s | %s | %s \n---+---+---\n %s | %s | %s \n---+---+---\n %s | %s | %s \n\n", board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8])
+	fmt.Printf("\n  1   2   3 \nA %s | %s | %s \n ---+---+---\nB %s | %s | %s \n ---+---+---\nC %s | %s | %s \n\n", board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8])
 }
 
 func (g *Game) InputFieldIndex() int {
@@ -24,9 +24,28 @@ func (g *Game) InputFieldIndex() int {
 	for index < 0 || index > 8 || g.board[index] != cellEmpty {
 		fmt.Print("Which field do you want to mark? ")
 		if scanner.Scan() {
-			i, err := strconv.Atoi(scanner.Text())
-			if err == nil {
-				index = i
+			parts := strings.Split(strings.ToLower(scanner.Text()), "")
+			if len(parts) == 2 {
+				row := -1
+				column := -1
+				for i := 0; i < 2; i++ {
+					if strings.ContainsAny(parts[i], "123") {
+						column, _ = strconv.Atoi(parts[i])
+						column--
+					} else if strings.ContainsAny(parts[i], "abc") {
+						switch parts[i] {
+						case "a":
+							row = 0
+						case "b":
+							row = 1
+						case "c":
+							row = 2
+						}
+					}
+				}
+				if row != -1 && column != -1 {
+					index = row*3 + column
+				}
 			}
 		}
 	}
